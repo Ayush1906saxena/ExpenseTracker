bb61d0c3be1c8e6b3288692b7878763cc6f37541
Ayush1906saxena <ayushsaxenarockz1906@gmail.com>
2022-06-12 10:47:42 +0530

    Added a hook for title change

--- src/components/UI/Card.js
+++ src/components/UI/Card.js
@@ -1,6 +1,7 @@
 import "./Card.css";
 
 function Card(props) {
+  // here we are adding two classes to our component, one is card class and then its own class
   const classes = "card " + props.className;
 
   return <div className={classes}>{props.children}</div>;
